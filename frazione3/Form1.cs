782a2cb5d7b0ad14334e4da4e67802632925385f
Utente <Utente@PC-DI-SIMO>
2024-02-13 22:15:50 +0100

    Aggiungere i file di progetto.

---
+++ frazione3/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frazione3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Fraction somma = Fraction.Parse("0/1");
+            foreach(Fraction f in listBox1.Items)
+            {
+                somma = somma + f;
+            }
+            label1.Text = somma.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Fraction primo = (Fraction)listBox1.Items[0];
+
+            for (int i = 1; i < listBox1.Items.Count; i++)
+            {
+                primo = primo - (Fraction)listBox1.Items[i];
+            }
+
+            label1.Text = primo.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Fraction moltiplica = Fraction.Parse("1/1");
+            foreach (Fraction f in listBox1.Items)
+            {
+                moltiplica = moltiplica * f;
+            }
+            label1.Text = moltiplica.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Fraction p = (Fraction)listBox1.Items[0];
+            for (int i = 1; i < listBox1.Items.Count; i++)
+            {
+                p = p / (Fraction)listBox1.Items[1];
+            }
+            
+            label1.Text = p.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Fraction f = Fraction.Parse(textBox1.Text);
+            listBox1.Items.Add(f);
+
+        }
+    }
+}
---
+++ frazione3/Fraction (2).cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace frazione3
+
+{
+    public class Fraction
+    {
+        public int N { get; }
+        public int D { get; }
+
+        public Fraction(int n, int d)
+        {
+            Semplifica(ref n, ref d);
+
+            N = n;
+            D = d;
+        }
+
+        private void Semplifica(ref int n, ref int d)
+        {
+            int mcd = CalcolaMcd(n, d);
+            n = n / mcd;
+            d = d / mcd;
+        }
+
+        private int CalcolaMcd(int a, int b)
+        {
+            while (b != 0)
+            {
+                int resto = a % b;
+                a = b;
+                b = resto;
+            }
+            return a;
+        }
+
+        public static Fraction operator+(Fraction a,Fraction other)
+        {
+            return new Fraction(a.N * other.D + a.D * other.N, a.D * other.D);
+        }
+
+        public static Fraction operator-(Fraction a, Fraction other)
+        {
+            return new Fraction(a.N * other.D - a.D * other.N, a.D * other.D);
+        }
+
+        public static Fraction operator*(Fraction a, Fraction other)
+        {
+            return new Fraction(a.N * other.N, a.D * other.D);
+        }
+
+        public static Fraction operator/(Fraction a, Fraction other)
+        {
+            return new Fraction(a.N * other.D, a.D * other.N);
+        }
+        
+        public static Fraction operator-(Fraction a)
+        {
+            return new Fraction(-a.N, a.D);
+        }
+
+
+        public override string ToString()
+        {
+            return $"{N}/{D}";
+        }
+
+
+        public static Fraction Parse(string s)
+        {
+            string[] parts = s.Split('/');
+            return new Fraction(int.Parse(parts[0]), int.Parse(parts[1]));
+        }
+    }
+}
